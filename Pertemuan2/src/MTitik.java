765461ceb6e64d98cb49595783eb0defb4c07009
ariefinsulthon-cmyk <ariefinsulthon@gmail.com>
2026-02-28 20:45:41 +0700

    Pertemuan 2 done

--- Pertemuan2/src/MTitik.java
+++ Pertemuan2/src/MTitik.java
@@ -1,5 +1,5 @@
 /*  Nama File : MTitik.java
-    Deskripsi : Berisi atribut dan method dalam class Titik
+    Deskripsi : Berisi implementasi dari class Titik
     Pembuat : Mohammad Sulthon Ariefin
     Tanggal : 19 Feb 2026 */
 
@@ -7,20 +7,25 @@ public class MTitik {
         public static void main(String[] args) {
             Titik T1 = new Titik();
             T1.printTitik();
-            T1.geser(3,4);
+            Titik.getCounterTitik();
+            T1 = new Titik(2,0);
             T1.printTitik();
-            Titik T2 = new Titik(6,8);
-            Titik T3 = new Titik();
-            T2.printTitik();
-            T1.setAbsis(10);
-            T1.setOrdinat(10);
-            T2.printTitik();
-            System.out.println(T1.getKuadran());
-            System.out.println(T2.getJarakPusat());
-            T2.refleksiX();
-            T2.printTitik();
-            T2.refleksiY();
-            T2.printTitik();
+            System.err.println(T1.getAbsis());
+            System.err.println(T1.getOrdinat());
+            T1.setAbsis(4);
+            T1.setOrdinat(6);
+            T1.printTitik();
+            T1.geser(2,2);
+            T1.printTitik();
+            System.out.println("Kuadran: " + T1.getKuadran());
+            System.out.println("Jarak Ke Pusat: " + T1.getJarakPusat());
+            Titik T2 = new Titik(8,8);
+            System.out.println("Jarak Ke Titik T2: " + T1.getJarak(T2));
+            T1.refleksiX();
+            T1.printTitik();
+            T1.refleksiY();
+            T1.printTitik();
             T1.getRefleksiX().printTitik();
+            T1.getRefleksiY().printTitik();
         }
     }
